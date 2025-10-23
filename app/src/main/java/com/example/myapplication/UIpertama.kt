59282fe67b6e1710e2047127e8345edcaaae942a
icad08 <irsyadrm08@gmail.com>
2025-10-23 20:19:51 +0700

    menambahkan card dan contentnya

--- app/src/main/java/com/example/myapplication/UIpertama.kt
+++ app/src/main/java/com/example/myapplication/UIpertama.kt
@@ -114,6 +114,13 @@ fun ActivitasPertama(modifier: Modifier){
                 }
             }
         }
+        Card ( modifier = Modifier
+            .fillMaxWidth(fraction = 1f)
+            .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Green
+            )
+        )
         Box(
             modifier = Modifier
                 .fillMaxSize()
