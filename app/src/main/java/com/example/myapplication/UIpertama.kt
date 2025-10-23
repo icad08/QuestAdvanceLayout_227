c84ed66e80096c4b0341f2698833004dd2be809a
icad08 <irsyadrm08@gmail.com>
2025-10-23 20:14:56 +0700

    menambahkan spacer dan column beserta contentnya

--- app/src/main/java/com/example/myapplication/UIpertama.kt
+++ app/src/main/java/com/example/myapplication/UIpertama.kt
@@ -92,6 +92,16 @@ fun ActivitasPertama(modifier: Modifier){
                         .size(size = 100.dp)
                         .padding(all = 5.dp)
                 )
+                Spacer(modifier = Modifier.width(width = 30.dp))
+                Column() {
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Default,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                }
             }
         }
         Box(
@@ -108,4 +118,5 @@ fun ActivitasPertama(modifier: Modifier){
     }
 
 }
+
 
