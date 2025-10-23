1bcef22affff98cfeb0f0f664dd41acf224e6d36
icad08 <irsyadrm08@gmail.com>
2025-10-23 20:15:28 +0700

    menambahkan text

--- app/src/main/java/com/example/myapplication/UIpertama.kt
+++ app/src/main/java/com/example/myapplication/UIpertama.kt
@@ -101,6 +101,11 @@ fun ActivitasPertama(modifier: Modifier){
                         color = Color.White,
                         modifier = Modifier.padding(top = 15.dp)
                     )
+                    Text(
+                        text = stringResource(id = R.string.alamat1), // Alamat dari strings.xml
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                    )
                 }
             }
         }
